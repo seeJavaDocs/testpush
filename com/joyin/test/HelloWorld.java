d973b170896dc19a401183bb0abe5eeff1b1494f
wudong_glb <wudong308_glb@qq.com>
2019-04-21 15:40:40 +0800

    clone项目后修改提交测试

--- com/joyin/test/HelloWorld.java
+++ com/joyin/test/HelloWorld.java
@@ -1,7 +1,10 @@
+package com.joyin.test;
+
 /**
  *这是wuletong账号修改的aaaabbb
  */
-public class hellowrld{
+public class HelloWorld {
+
 	public static void main (String[] args){
 		System.out.println("hello world!!");//edit by testclone
 		System.out.println("hello world!!");//edit by testpush
